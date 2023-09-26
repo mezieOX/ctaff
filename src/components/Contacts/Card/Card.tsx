52e661230fa453731263d1c841f58841c1237584
mezieOX <meziepage692@gmail.com>
2023-09-26 11:49:10 +0100

    done with the color implementation and some correction was made

--- src/components/Contacts/Card/Card.tsx
+++ src/components/Contacts/Card/Card.tsx
@@ -19,7 +19,7 @@ const Card = () => {
       maxWidth={{ base: "100%", xl: "35%" }}
       _hover={{
         boxShadow:
-          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 5px 0 rgba(0, 0, 0, 0.19)",
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
       }}
       top={{ base: 0, xl: "-5rem" }}
       left={{ base: "0", xl: "4.5rem" }}
